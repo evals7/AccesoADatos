4def8f0fa279c8608f185379a2d3712fe6976b9b
evals7 <eva.lopez269@comunidadunir.net>
2025-11-05 23:12:32 +0100

    Creación de métodos CRUD y métodos de controller para separar la logica de negocio de la logica de bbdd

---
+++ Tema 2. FicherosXML/Parseador XML/src/Entrada.java
@@ -0,0 +1,7 @@
+public class Entrada {
+    public static void main(String[] args) {
+        OperacionesXML operacionesXML = new OperacionesXML();
+        //operacionesXML.lecturaXML();
+        operacionesXML.escrituraXML();
+    }
+}
